226edb3b2c241977fbf8a3a92d421901c82ddfc8
AhaduH <ahaile1357@gmail.com>
2026-03-31 13:27:14 -0400

    added simple clickable power menu

---
+++ waybar/powermenu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+confirm () {
+    printf "No\nYes" | fuzzel --width 20 --lines 7 --dmenu
+}
+
+choice=$(printf "Lock\nLogout\nReboot\nShutdown" | fuzzel --width 20 --lines 7 --dmenu)
+
+case "$choice" in
+    Lock) hyprlock ;;
+    Suspend) hyprlock && systemctl suspend ;;
+    #Logout) hyprctl dispatch exit ;; # Won't reallly use this
+    Reboot) [[ $(confirm) == "Yes" ]] && systemctl reboot ;;
+    Shutdown) [[ $(confirm) == "Yes" ]] && systemctl poweroff ;;
+esac
